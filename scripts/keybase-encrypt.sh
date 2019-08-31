e12bbfe64bde54cb692e06b891a6a027dbf09747
Tim Gatzemeier <etwillbefine@users.noreply.github.com>
2019-08-31 20:47:44 +0200

    remove standalone flag
    it does not seem to be mandatory when running on a properly configured alpine image

--- scripts/keybase-encrypt.sh
+++ scripts/keybase-encrypt.sh
@@ -6,7 +6,7 @@ set -e
 eval "$(jq -r '@sh "KEYBASE_USERNAME=\(.keybase) PRIVATE_KEY=\(.private_key)"')"
 
 # Encrypt from stdin
-encrypted=$(echo "$PRIVATE_KEY" | keybase --standalone pgp encrypt --no-self ${KEYBASE_USERNAME} | base64)
+encrypted=$(echo "$PRIVATE_KEY" | keybase pgp encrypt --no-self ${KEYBASE_USERNAME} | base64)
 
 # Build result json
 jq -n --arg result "$encrypted" '{"key": $result}'
